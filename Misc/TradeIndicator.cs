64d98258ebad1db45f6ab2bd56e1fa3bb4e4ddef
Sebastien Dubos <sebastien.dubos@ecdc.europa.eu>
2022-05-13 15:30:47 +0200

    feat : added try catch block on Indicators

--- Misc/TradeIndicator.cs
+++ Misc/TradeIndicator.cs
@@ -8,7 +8,6 @@ namespace MarginCoin.Misc
 {
     public static class TradeIndicator
     {
-
         public static void CalculateIndicator<T>(ref List<T> quotationList) where T : Candle
         {
             var data = quotationList.Select(p => p.c).ToArray();
@@ -20,27 +19,49 @@ namespace MarginCoin.Misc
             double[] outMACDHist = new double[data.Length];
             double[] emaValues = new double[data.Length];
 
-            //Calculate RSI
-            var statusRsi = Core.Rsi(0, data.Length - 1, data, 14, out beginIndex, out outNBElements, rsiValues);
-            if (statusRsi == Core.RetCode.Success && outNBElements > 0)
+            try
             {
-                for (int i = 0; i < quotationList.Count-14; i++)
+                var statusRsi = Core.Rsi(0, data.Length - 1, data, 14, out beginIndex, out outNBElements, rsiValues);
+                if (statusRsi == Core.RetCode.Success && outNBElements > 0)
                 {
-                    quotationList[i + 14].Rsi = rsiValues[i];
+                    for (int i = 0; i < quotationList.Count - 14; i++)
+                    {
+                        quotationList[i + 14].Rsi = rsiValues[i];
+                    }
                 }
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("RSI calulation failled!");
+            }
+            finally
+            {
+                
             }
 
             //Calculate MACD
-            // var statusMacd = Core.Macd(0, data.Length - 1, data, 12, 26, 9, out beginIndex, out outNBElements, outMACD, outMACDSignal, outMACDHist);
-            // if (statusMacd == Core.RetCode.Success && outNBElements > 0 )
-            // {
-            //     for (int i = 0; i < quotationList.Count - 33; i++)
-            //     {
-            //         quotationList[i + 33].Macd = outMACD[i];
-            //         quotationList[i + 33].MacdHist = outMACDHist[i];
-            //         quotationList[i + 33].MacdSign = outMACDSignal[i];
-            //     }
-            // }
+            try
+            {
+                var statusMacd = Core.Macd(0, data.Length - 1, data, 12, 26, 9, out beginIndex, out outNBElements, outMACD, outMACDSignal, outMACDHist);
+                if (statusMacd == Core.RetCode.Success && outNBElements > 0)
+                {
+                    for (int i = 0; i < quotationList.Count - 33; i++)
+                    {
+                        quotationList[i + 33].Macd = outMACD[i];
+                        quotationList[i + 33].MacdHist = outMACDHist[i];
+                        quotationList[i + 33].MacdSign = outMACDSignal[i];
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("RSI calulation failled!");
+            }
+            finally
+            {
+                
+            }
+
 
             // //Calculate EMA50
             // var statusEma = Core.Ema(0, data.Length - 1, data, 50, out beginIndex, out outNBElements, emaValues);
